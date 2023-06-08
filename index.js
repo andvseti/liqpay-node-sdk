f37da478606d268027d21a659455219ba544a84b
Андрей <andrei.vseti@gmail.com>
2023-06-08 11:45:08 +0300

    liqPayPayStatuses export

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const LiqPay = require('./classes/LiqPay');
 const LiqPayAPI = require('./classes/LiqPayAPI');
 const LiqPayDataPreparer = require('./classes/LiqPayDataPreparer');
+const paymentStatusesMap = require('./classes/paymentStatusesMap');
 
 module.exports = (options = {}) => new LiqPay(options);
 module.exports.LiqPay = LiqPay;
 module.exports.API = LiqPayAPI;
 module.exports.DataPreparer = LiqPayDataPreparer;
+module.exports.liqPayPayStatuses = paymentStatusesMap;
